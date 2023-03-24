84ff6f4731b0be698d6f67767b74207087bf438e
ajin3830 <ajin3830@gmail.com>
2023-03-24 14:29:30 -0400

    passed all test

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Hero
+from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -21,6 +21,66 @@ api = Api(app)
 def home():
     return ''
 
+# GET /heroes
+@app.route('/heroes', methods=['GET'])
+def heroes():
+    heroes = Hero.query.all()
+    return make_response([hero.to_dict(rules= ('-hero_powers', '-updated_at', '-created_at')) for hero in heroes], 200)
 
+# GET /heroes/int:id
+@app.route('/heroes/<int:id>', methods=['GET'])
+def hero_by_id(id):
+    hero = Hero.query.filter_by(id=id).first()
+    if hero:
+        return make_response(hero.to_dict(rules = ('-hero_powers', '-created_at', '-updated_at')), 200)
+    return make_response({'error': 'Hero not found'}, 404)
+
+# GET /powers
+@app.route('/powers', methods=['GET'])
+def powers():
+    powers = Power.query.all()
+    return make_response([power.to_dict(rules = ('-created_at', '-hero_powers', '-updated_at')) for power in powers], 200)
+
+# GET, PATCH /powers/int:id
+@app.route('/powers/<int:id>', methods=['GET', 'PATCH'])
+def power_by_id(id):
+    power = Power.query.filter_by(id=id).first()
+    if power:
+        if request.method == 'GET':
+            return make_response(power.to_dict(rules = (('-created_at', '-hero_powers', '-updated_at'))), 200)
+        elif request.method == 'PATCH':
+            try:
+                for key in request.get_json():
+                    setattr(power, key, request.get_json()[key])
+                # power.name=request.get_json()['name']
+                # power.description=request.get_json()['description']
+                
+                db.session.add(power)
+                db.session.commit()
+
+                return make_response(power.to_dict(), 200)
+            except ValueError:
+                return make_response({"error": "Invalid input"}, 400)
+            
+    return make_response({'error': 'Power not found'}, 404)     
+    
+
+# POST /hero_powers
+@app.route('/hero_powers', methods=['POST'])
+def hero_powers():
+    try:
+        new_hp = HeroPower(
+            strength = request.get_json()['strength'],
+            hero_id = request.get_json()['hero_id'],
+            power_id = request.get_json()['power_id']
+        )
+        db.session.add(new_hp)
+        db.session.commit()
+        # now new_hp is in database, retrieve and show it
+        hero = Hero.query.filter_by(id=new_hp.hero_id).first()
+        return make_response(hero.to_dict(rules = ('-created_at', '-updated_at')), 201)
+    except ValueError:
+        return make_response({"error": "Invalid input"}, 400)
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,10 +1,80 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+db = SQLAlchemy(metadata=metadata)
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
+
+    serialize_rules = ('-hero_powers.power', "-hero_powers.hero")
+    # serialize_rules = ('-hero_powers.hero', '-powers.heroes')
+
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# add any models you may need. 
+    hero_powers = db.relationship('HeroPower', backref='hero')
+
+    powers = association_proxy('hero_powers', 'power')
+
+    def __repr__(self):
+        return f'<Hero {self.name} {self.super_name} />'
+
+class Power(db.Model, SerializerMixin):
+    __tablename__ = 'powers'
+
+    serialize_rules = ("-hero_powers.power", "-hero_powers.hero")
+    # serialize_rules = ('-hero_powers.power', '-heroes.powers')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if not description or len(description)< 20:
+            raise ValueError('Must have a description with at least 20 characters long')
+        return description
+
+    hero_powers = db.relationship('HeroPower', backref='power')
+
+    heroes = association_proxy('hero_powers', 'hero')
+
+    def __repr__(self):
+        return f'<Power {self.name} {self.description} />'
+    
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__ = 'hero_powers'
+    # one to many singular to plural
+    serialize_rules = ('-hero.hero_powers', "-power.hero_powers", "-heroes.powers", "-powers.heroes")
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())   
+
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    @validates('strength')
+    def validate_strength(self, key, strength):
+        # if not strength =='Strong' and not strength =='Weak' and not strength == 'Average'
+        values = ['Strong', 'Weak', 'Average']
+        if strength not in values:
+            raise ValueError('Must be strong, weak, or average')
+        return strength
+    
+    def __repr__(self):
+        return f'<HeroPower {self.strength} />' 
